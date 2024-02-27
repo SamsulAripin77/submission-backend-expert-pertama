12f974ebedc4baca0e09b1286775606cab2c7ba0
SamsulAripin77 <arifins192@gmail.com>
2024-02-27 11:10:17 +0700

    error get detail thread

--- src/Infrastructures/http/_test/threads.test.js
+++ src/Infrastructures/http/_test/threads.test.js
@@ -270,7 +270,7 @@ describe('/threads endpoint', () => {
       
       const responseJson = JSON.parse(response.payload)
       console.log(responseJson);
-      // expect(response.statusCode).toEqual(200)
+      expect(response.statusCode).toEqual(200)
       // expect(responseJson.status).toEqual('success')
       // expect(responseJson.data.thread.comments).toBeDefined()
     })
